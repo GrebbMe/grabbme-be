185d4a80130dc756a3ada143bc07fe6abcd810a3
YKDman <dbsrudejr300@gmail.com>
2024-09-10 17:40:46 +0900

    refactor : 미사용 import 제거

--- apps/api-gateway/src/users/users.controller.ts
+++ apps/api-gateway/src/users/users.controller.ts
@@ -10,7 +10,6 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiConsumes, ApiExtraModels, ApiTags } from '@nestjs/swagger';
 
 // swagger 적는 곳
 
