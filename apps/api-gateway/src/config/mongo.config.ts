cb297f0a159933cc1125ffe42e6dfca1fc9bd922
YKDman <dbsrudejr300@gmail.com>
2024-09-10 17:23:37 +0900

    refactor : config.ts 매직넘버 제거

--- apps/api-gateway/src/config/mongo.config.ts
+++ apps/api-gateway/src/config/mongo.config.ts
@@ -1,8 +1,8 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('mongo', () => ({
-  host: process.env.MONGO_HOST || 'mongodb://svc.sel4.cloudtype.app',
-  port: process.env.MONGO_PORT ? Number(process.env.MONGO_PORT) : 32138,
-  username: process.env.MONGO_USERNAME || 'root',
-  password: process.env.MONGO_PASSWORD || 'grabb',
+  host: process.env.MONGO_HOST,
+  port: Number(process.env.MONGO_PORT),
+  username: process.env.MONGO_USERNAME,
+  password: process.env.MONGO_PASSWORD,
 }));
--- apps/users/src/config/mysql.config.ts
+++ apps/users/src/config/mysql.config.ts
@@ -1,9 +1,9 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('mysql', () => ({
-  host: process.env.MYSQL_HOST || 'svc.sel4.cloudtype.app',
-  port: process.env.MYSQL_PORT ? Number(process.env.MYSQL_PORT) : 32528,
-  username: process.env.MYSQL_USERNAME || 'root',
-  password: process.env.MYSQL_PASSWORD || 'grebb',
-  database: process.env.MYSQL_DATABASE || 'grabb',
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 }));
