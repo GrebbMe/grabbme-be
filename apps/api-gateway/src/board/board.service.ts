996d8eaf99a3242c5b0ebe80fddb701dfaceed09
YKDman <dbsrudejr300@gmail.com>
2024-09-11 21:43:02 +0900

    refactor : api-gateway board service ClientProxy패턴 생성

--- apps/api-gateway/src/board/board.service.ts
+++ apps/api-gateway/src/board/board.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class BoardService {
+  constructor(
+    @Inject('BOARD_SERVICE') private readonly boardClient: ClientProxy,
+  ) {}
   create(createBoardDto: CreateBoardDto) {
     return 'This action adds a new board';
   }
