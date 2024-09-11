05aa67b91ff7dd35ae013efecb21fb67afe50ca5
YKDman <dbsrudejr300@gmail.com>
2024-09-11 17:19:23 +0900

    refactor : ESLint 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,8 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'on',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/ban-types': 'on',
   },
 };
