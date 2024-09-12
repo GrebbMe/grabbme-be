3c793b731666032fb1c0173582ec1f3602274b55
YKDman <dbsrudejr300@gmail.com>
2024-09-12 20:36:32 +0900

    refactor : port.config.ts 삭제

--- apps/api-gateway/src/config/port.config.ts
+++
@@ -1,7 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('port', () => ({
-  gateway: Number(process.env.GATEWAY_PORT),
-  user: Number(process.env.USER_PORT),
-  board: Number(process.env.BOARD_PORT),
-}));
