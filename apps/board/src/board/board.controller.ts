a3e827821b288bd5fec5513ea3c1f2badeb1065a
YKDman <dbsrudejr300@gmail.com>
2024-09-15 22:09:30 +0900

    config : husky lint-staged 설정

--- apps/board/src/board/board.controller.ts
+++ apps/board/src/board/board.controller.ts
@@ -3,10 +3,5 @@ import { BoardService } from './board.service';
 
 @Controller()
 export class BoardController {
-  public constructor(private readonly boardService: BoardService) {}
-
-  // @MessagePattern('createBoard')
-  // create(@Payload() createBoardDto: CreateBoardDto) {
-  //   return this.boardService.create(createBoardDto);
-  // }
+	public constructor(private readonly boardService: BoardService) {}
 }
