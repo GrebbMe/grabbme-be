5431c07bbca01631f367f71b378e478b9d08e6fd
YKDman <dbsrudejr300@gmail.com>
2024-09-16 00:59:29 +0900

    config : husky 설정

--- apps/board/src/main.ts
+++ apps/board/src/main.ts
@@ -3,19 +3,16 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = Number(process.env.BOARD_PORT);
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-      options: {
-        host: 'localhost', // 'board-service',
-        port: PORT,
-      },
-    },
-  );
+	const PORT = Number(process.env.BOARD_PORT);
+	const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+		transport: Transport.TCP,
+		options: {
+			host: 'localhost', // 'board-service',
+			port: PORT,
+		},
+	});
 
-  await app.listen();
-  console.info(`board-service Running On ${PORT} for TCP`);
+	await app.listen();
+	console.info(`board-service Running On ${PORT} for TCP`);
 }
 bootstrap();
