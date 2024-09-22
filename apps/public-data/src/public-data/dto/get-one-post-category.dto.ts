94008b28b832973d7083a69f9ac6d5ae0fc4fd5f
YKDman <dbsrudejr300@gmail.com>
2024-09-22 21:17:23 +0900

    refactor : GetOnePostCategoryDto IsNumber -> IsInt 변경

--- apps/public-data/src/public-data/dto/get-one-post-category.dto.ts
+++ apps/public-data/src/public-data/dto/get-one-post-category.dto.ts
@@ -1,8 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber } from 'class-validator';
+import { IsInt } from 'class-validator';
 
 export class GetOnePostCategoryDto {
-  @IsNumber()
+  @IsInt()
   @ApiProperty({ description: 'post_category ID' })
   public id: number;
 }
