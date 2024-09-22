b3fc79bf0009984f82be23f8f1752760a0ec132f
YKDman <dbsrudejr300@gmail.com>
2024-09-22 23:24:57 +0900

    refactor : MessagePattern 상수 구조 변경 (객체 리터럴)

--- shared/constants/message-pattern.ts
+++ shared/constants/message-pattern.ts
@@ -1,11 +1,8 @@
-export enum MessagePatternEnum {
-  GET_ALL_POST_CATEGORY = 'get-all-post-category',
-  GET_ONE_POST_CATEGORY = 'get-one-post-category',
-}
-
-export const publicDataMessages = {
-  POST_CATEGORY: {
-    GET_ALL_POST_CATEGORY: { cmd: MessagePatternEnum.GET_ALL_POST_CATEGORY },
-    GET_ONE_POST_CATEGORY: { cmd: MessagePatternEnum.GET_ONE_POST_CATEGORY },
+export const MESSAGE = {
+  PUBLIC_DATA: {
+    POST_CATEGORY: {
+      GET_ALL_POST_CATEGORY: { cmd: 'get-all-post-category' },
+      GET_ONE_POST_CATEGORY: { cmd: 'get-one-post-category' },
+    },
   },
 } as const;
