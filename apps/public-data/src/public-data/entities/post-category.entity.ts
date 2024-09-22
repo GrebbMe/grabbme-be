1a670c3261e307094551b8416b1ca76055058a9c
YKDman <dbsrudejr300@gmail.com>
2024-09-22 21:10:30 +0900

    refactor : Enitity 내 ApiProperty 삭제

--- apps/public-data/src/public-data/entities/post-category.entity.ts
+++ apps/public-data/src/public-data/entities/post-category.entity.ts
@@ -1,13 +1,10 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('post_category')
 export class PostCategory {
   @PrimaryGeneratedColumn()
-  @ApiProperty({ description: 'post_category ID' })
   public id: number;
 
   @Column()
-  @ApiProperty({ description: 'post_category name' })
   public post_category_name: string;
 }
