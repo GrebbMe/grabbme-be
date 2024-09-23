3025ba86c84cb6a2fffc41c5360921022dcedb6b
una3325 <a33256448@gmail.com>
2024-09-24 06:16:12 +0900

    feat : api-gateway 게시글 CRUD API 구현

--- apps/api-gateway/src/board/board.controller.ts
+++ apps/api-gateway/src/board/board.controller.ts
@@ -1,7 +1,55 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Body, Patch } from '@nestjs/common';
 import { BoardService } from './board.service';
+import { ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { UpdateBoardDto } from './dto/update-board.dto';
 
 @Controller('board')
 export class BoardController {
   public constructor(private readonly boardService: BoardService) {}
+
+  @Get()
+  @ApiOperation({ summary: '전체 게시글 조회' })
+  @ApiCreatedResponse({
+    description: '전체 게시글 조회',
+  })
+  public async getAllBoards() {
+    return await this.boardService.getAllBoards();
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: '게시글 상세 조회' })
+  @ApiCreatedResponse({
+    description: '게시글 상세 조회',
+  })
+  public async getOneBoard(@Param('id') postId: string) {
+    return await this.boardService.getOneBoard(postId);
+  }
+
+  @Post()
+  @ApiOperation({ summary: '게시글 생성' })
+  @ApiCreatedResponse({
+    description: '게시글 생성',
+  })
+  public async createBoard(@Body() createBoardDto: CreateBoardDto) {
+    return await this.boardService.createBoard(createBoardDto);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: '게시글 수정' })
+  @ApiCreatedResponse({
+    description: '게시글 수정',
+  })
+  public async updateBoard(@Param('id') updateId: string, @Body() updateData: UpdateBoardDto) {
+    return await this.boardService.updateBoard(updateId, updateData);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: '게시글 삭제' })
+  @ApiCreatedResponse({
+    description: '게시글 삭제',
+  })
+  public async deleteBoard(@Param('id') deleteId: string) {
+    return await this.boardService.deleteBoard(deleteId);
+  }
 }
--- apps/api-gateway/src/board/board.service.ts
+++ apps/api-gateway/src/board/board.service.ts
@@ -1,7 +1,29 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { UpdateBoardDto } from './dto/update-board.dto';
 
 @Injectable()
 export class BoardService {
   public constructor(@Inject('BOARD_SERVICE') private readonly boardClient: ClientProxy) {}
+
+  public async getAllBoards() {
+    return await this.boardClient.send({ cmd: 'get-all-boards' }, {});
+  }
+
+  public async getOneBoard(id: string) {
+    return await this.boardClient.send({ cmd: 'get-one-board' }, { id });
+  }
+
+  public async createBoard(createBoardDto: CreateBoardDto) {
+    return await this.boardClient.send({ cmd: 'create-board' }, { createBoardDto });
+  }
+
+  public async updateBoard(id: string, updateData: UpdateBoardDto) {
+    return await this.boardClient.send({ cmd: 'update-board' }, { id, updateData });
+  }
+
+  public async deleteBoard(id: string) {
+    return await this.boardClient.send({ cmd: 'delete-board' }, { id });
+  }
 }
