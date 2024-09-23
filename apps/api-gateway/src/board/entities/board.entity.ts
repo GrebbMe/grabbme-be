cdb017cc73aeafd004492fa349dfe26f04efc238
una3325 <a33256448@gmail.com>
2024-09-24 06:46:05 +0900

    feat : api-gateway 게시글 Entity 정의

--- apps/api-gateway/src/board/entities/board.entity.ts
+++ apps/api-gateway/src/board/entities/board.entity.ts
@@ -1,1 +1,63 @@
-export class Board {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('posts')
+export class Board {
+  @PrimaryGeneratedColumn()
+  public post_id: number;
+
+  @Column({ type: 'varchar' })
+  public title: string;
+
+  @Column({ type: 'text' })
+  public content: string;
+
+  @Column({ type: 'timestamp' })
+  public update_at: Date;
+
+  @Column({ type: 'timestamp' })
+  public create_at: Date;
+
+  @Column({ type: 'timestamp' })
+  public expired_at: Date;
+
+  @Column({ type: 'int', default: 0 })
+  public view_cnt: number;
+
+  @Column({ type: 'int', default: 0 })
+  public bookmarked_cnt: number;
+
+  @Column({ type: 'boolean', default: true })
+  public is_open: boolean;
+
+  // @ManyToOne(() => users)
+  // @JoinColumn({name:'user_id'})
+  // public user: User;
+
+  // @ManyToOne(() => teams)
+  // @JoinColumn({name:'team_id'})
+  // public team: Team;
+
+  // @ManyToOne(() => PostCategory)
+  // @JoinColumn({name:'post_category_id'})
+  // public postCategory : PostCategory;
+
+  // @ManyToOne(() => ProjectCategory)
+  // @JoinColumn({name:'project_category_id'})
+  // public projectCategory : ProjectCategory;
+
+  // @ManyToOne(() => JobCategory)
+  // @JoinColumn({name:'job_category_id'})
+  // public jobCategory : JobCategory;
+
+  // @ManyToOne(() => StackCategory)
+  // @JoinColumn({name:'stack_category_id'})
+  // public stackCategory : StackCategory;
+
+  // @ManyToOne(() => CareerCategory)
+  // @JoinColumn({name:'career_category_id'})
+  // public careerCategory : CareerCategory;
+
+  // @ManyToOne(() => Attachment)
+  // @JoinColumn({name:'attachment_id'})
+  // public attachment : Attachment;
+}
