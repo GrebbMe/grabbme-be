7c5e7fb2c821a67902c23fcde0bd1f1f542f2e2d
una3325 <a33256448@gmail.com>
2024-09-24 13:30:10 +0900

    feat : MicroService 게시글 DTO 추가

--- apps/board/src/board/dto/create-board.dto.ts
+++ apps/board/src/board/dto/create-board.dto.ts
@@ -1,1 +1,25 @@
-export class CreateBoardDto {}
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateBoardDto {
+  @IsString()
+  public title: string;
+
+  @IsString()
+  public content: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  public expired_at: Date;
+
+  // @IsNumber()
+  // @IsNotEmpty()
+  // public project_category_id: number;
+
+  // @IsNumber()
+  // @IsNotEmpty()
+  // public stack_category_id: number;
+
+  // @IsString()
+  // @IsNotEmpty()
+  // public teams: string;
+}
