3461fd301d57c330d714ae320ef4848fc483d96f
una3325 <a33256448@gmail.com>
2024-09-24 13:25:52 +0900

    feat : MicroService 게시글 CRUD API 구현

--- apps/board/src/board/board.controller.ts
+++ apps/board/src/board/board.controller.ts
@@ -1,7 +1,35 @@
 import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { BoardService } from './board.service';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { UpdateBoardDto } from './dto/update-board.dto';
 
-@Controller()
+@Controller('board')
 export class BoardController {
   public constructor(private readonly boardService: BoardService) {}
+
+  @MessagePattern({ cmd: 'get-all-boards' })
+  public async getAllBoards() {
+    return this.boardService.getAllBoards();
+  }
+
+  @MessagePattern({ cmd: 'get-one-board' })
+  public async getOneBoard(@Payload() data: { id: number }) {
+    return this.boardService.getOneBoard(data.id);
+  }
+
+  @MessagePattern({ cmd: 'create-board' })
+  public async createBoard(@Payload() data: CreateBoardDto) {
+    return this.boardService.createBoard(data);
+  }
+
+  @MessagePattern({ cmd: 'update-board' })
+  public async updateBoard(@Payload() data: { id: number; updateData: UpdateBoardDto }) {
+    return this.boardService.updateBoard(data.id, data.updateData);
+  }
+
+  @MessagePattern({ cmd: 'delete-board' })
+  public async deleteBoard(@Payload() data: { id: number }) {
+    return this.boardService.deleteBoard(data.id);
+  }
 }
--- apps/board/src/board/board.service.ts
+++ apps/board/src/board/board.service.ts
@@ -1,4 +1,109 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { UpdateBoardDto } from './dto/update-board.dto';
+import { Board } from './entities/board.entity';
 
 @Injectable()
-export class BoardService {}
+export class BoardService {
+  public constructor(
+    @InjectRepository(Board)
+    private readonly boardRepository: Repository<Board>,
+    private readonly dataSource: DataSource,
+  ) {}
+
+  public async getAllBoards(): Promise<Board[]> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.startTransaction();
+    try {
+      const boards = await this.boardRepository.find();
+      await queryRunner.commitTransaction();
+      return boards;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  public async getOneBoard(id: number): Promise<Board> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.startTransaction();
+    try {
+      const board = await this.boardRepository.findOneBy({ post_id: id });
+      if (!board) {
+        throw new Error('게시글을 찾을 수 없습니다.');
+      }
+      await queryRunner.commitTransaction();
+      return board;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  public async createBoard(data: CreateBoardDto): Promise<Board> {
+    const newBoardData = {
+      title: data.title,
+      content: data.content,
+      expired_at: new Date(data.expired_at),
+    };
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.startTransaction();
+    try {
+      const newBoard = this.boardRepository.create(newBoardData);
+      const savedBoard = await queryRunner.manager.save(newBoard);
+      await queryRunner.commitTransaction();
+      return savedBoard;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  public async updateBoard(id: number, updateBoardDto: UpdateBoardDto): Promise<Board> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.startTransaction();
+    try {
+      const board = await this.boardRepository.findOneBy({ post_id: id });
+      if (!board) {
+        throw new Error('게시글을 찾을 수 없습니다.');
+      }
+
+      const updatedBoard = this.boardRepository.merge(board, updateBoardDto);
+      const savedBoard = await queryRunner.manager.save(updatedBoard);
+      await queryRunner.commitTransaction();
+      return savedBoard;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  public async deleteBoard(id: number): Promise<void> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.startTransaction();
+    try {
+      const board = await this.boardRepository.findOneBy({ post_id: id });
+      if (!board) {
+        throw new Error('게시글을 찾을 수 없습니다.');
+      }
+
+      await queryRunner.manager.remove(Board, board);
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+}
