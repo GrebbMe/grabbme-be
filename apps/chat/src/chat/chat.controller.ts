8ebc4bedbb65d161340fa7a0596239140af2560f
yeopseung <kwak991021@gmail.com>
2024-09-25 16:02:05 +0900

    comment : 채팅 Controller, Gateway 주석 수정

--- apps/chat/src/chat/chat.controller.ts
+++ apps/chat/src/chat/chat.controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { ChatService } from './chat.service';
 
-//* HTTP
+//* 채팅 서비스의 HTTP 요청/응답 처리 Controller
 @Controller()
 export class ChatController {
   public constructor(private readonly chatService: ChatService) {}
--- apps/chat/src/chat/chat.gateway.ts
+++ apps/chat/src/chat/chat.gateway.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-//* WebSocket (포트 번호 생략. CHAT_PORT에서 WebSocket도 실행.)
+//* 채팅 서비스의 WebSocket 실시간 통신 처리 Gateway (포트 번호 생략. CHAT_PORT에서 WebSocket도 실행.)
 //TODO: CORS 설정.
 @WebSocketGateway({ cors: { origin: '*' } })
 export class ChatGateway {
