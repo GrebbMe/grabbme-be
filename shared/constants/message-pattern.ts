9667bf3d8b85e1ae8c5d45901945210cbbc13b2f
YKDman <dbsrudejr300@gmail.com>
2024-09-25 16:54:54 +0900

    feat : 메세지패턴 position-category 상수 패턴 추가

--- shared/constants/message-pattern.ts
+++ shared/constants/message-pattern.ts
@@ -4,5 +4,9 @@ export const MESSAGE = {
       GET_ALL_POST_CATEGORY: { cmd: 'get-all-post-category' },
       GET_ONE_POST_CATEGORY: { cmd: 'get-one-post-category' },
     },
+    POSITION_CATEGORY: {
+      GET_ALL_POSITION_CATEGORY: { cmd: 'get-all-position-category' },
+      GET_ONE_POSITION_CATEGORY: { cmd: 'get-one-position-category' },
+    },
   },
 } as const;
