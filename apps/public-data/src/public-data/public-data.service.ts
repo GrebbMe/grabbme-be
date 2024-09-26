ec3462161718f6283432bbc1c9f6923ec2f804a2
YKDman <dbsrudejr300@gmail.com>
2024-09-26 17:55:42 +0900

    feat : 트랜잭션 데코레이터 적용

--- apps/public-data/src/public-data/public-data.service.ts
+++ apps/public-data/src/public-data/public-data.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DataSource, Repository } from 'typeorm';
-import { PostCategory, PositionCategory } from './entities';
+import { Transactional } from 'typeorm-transactional';
+import { PositionCategory } from './entities';
+import { PostCategory } from './entities/post-category.entity';
 
 @Injectable()
 export class PublicDataService {
@@ -13,90 +15,41 @@ export class PublicDataService {
     private positionCategoryRepository: Repository<PositionCategory>,
   ) {}
 
+  @Transactional()
   public async getPostCategories(): Promise<PostCategory[]> {
-    const queryRunner = this.dataSource.createQueryRunner();
+    const postCategories = await this.postCategoryRepository.find();
 
-    await queryRunner.startTransaction();
+    if (postCategories.length === 0) throw new NotFoundException('데이터가 없습니다.');
 
-    try {
-      const postDatas = await this.postCategoryRepository.find();
-      await queryRunner.commitTransaction();
-
-      return postDatas;
-    } catch (err) {
-      await queryRunner.rollbackTransaction();
-
-      throw err;
-    } finally {
-      await queryRunner.release();
-    }
+    return postCategories;
   }
 
+  @Transactional()
   public async getPostCategoryById(id: number): Promise<PostCategory> {
-    const queryRunner = this.dataSource.createQueryRunner();
+    const postCategory = await this.postCategoryRepository.findOne({ where: { id } });
 
-    await queryRunner.startTransaction();
+    if (!postCategory) throw new NotFoundException('데이터가 없습니다.');
 
-    try {
-      const postData = await this.postCategoryRepository.findOne({ where: { id } });
-      if (!postData) {
-        throw new NotFoundException('데이터가 없습니다.');
-      }
-      await queryRunner.commitTransaction();
-
-      return postData;
-    } catch (err) {
-      await queryRunner.rollbackTransaction();
-
-      throw new NotFoundException('사용자를 찾을 수 없습니다.');
-    } finally {
-      await queryRunner.release();
-    }
+    return postCategory;
   }
 
+  @Transactional()
   public async getPositionCategories(): Promise<PositionCategory[]> {
-    const queryRunner = this.dataSource.createQueryRunner();
+    const positionCategories = await this.positionCategoryRepository.find();
 
-    await queryRunner.startTransaction();
-    try {
-      const positionCategories = await this.positionCategoryRepository.find();
+    if (positionCategories.length === 0) throw new NotFoundException('데이터가 없습니다.');
 
-      if (positionCategories.length === 0) {
-        throw new NotFoundException('데이터가 없습니다.');
-      }
-      await queryRunner.commitTransaction();
-
-      return positionCategories;
-    } catch (err) {
-      await queryRunner.rollbackTransaction();
-
-      return err;
-    } finally {
-      await queryRunner.release();
-    }
+    return positionCategories;
   }
 
+  @Transactional()
   public async getPositionCategoryById(id: number): Promise<PositionCategory> {
-    const queryRunner = this.dataSource.createQueryRunner();
+    const positionCategory = await this.positionCategoryRepository.findOne({
+      where: { position_category_id: id },
+    });
 
-    await queryRunner.startTransaction();
-    try {
-      const positionCategory = await this.positionCategoryRepository.findOne({
-        where: { position_category_id: id },
-      });
+    if (!positionCategory) throw new NotFoundException('데이터가 없습니다.');
 
-      if (!positionCategory) {
-        throw new NotFoundException('포지션 정보가 없습니다.');
-      }
-
-      await queryRunner.commitTransaction();
-
-      return positionCategory;
-    } catch (err) {
-      await queryRunner.rollbackTransaction();
-      return err;
-    } finally {
-      await queryRunner.release();
-    }
+    return positionCategory;
   }
 }
