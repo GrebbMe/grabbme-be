a9850f00ef4e700cbd381e1f89bf8c3111a5964f
YKDman <83913024+ykdman@users.noreply.github.com>
2024-09-26 18:44:15 +0900

    [FEAT] Transactional 데코레이터 적용 (#31)
    * package : typeorm-transactional 설치
    
    * feat : typeorm datasource 설정 부분 추가
    
    * feat : public-data transactional 데코레이터 적용

--- apps/public-data/src/public-data/public-data.service.ts
+++ apps/public-data/src/public-data/public-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DataSource, Repository } from 'typeorm';
+import { Transactional } from 'typeorm-transactional';
 import { PostCategory } from './entities/post-category.entity';
 
 @Injectable()
@@ -11,41 +12,21 @@ export class PublicDataService {
     private postCategoryRepository: Repository<PostCategory>,
   ) {}
 
+  @Transactional()
   public async getPostCategories(): Promise<PostCategory[]> {
-    const queryRunner = this.dataSource.createQueryRunner();
+    const postCategories = await this.postCategoryRepository.find();
 
-    await queryRunner.startTransaction();
+    if (postCategories.length === 0) throw new NotFoundException('데이터가 없습니다.');
 
-    try {
-      const postDatas = await this.postCategoryRepository.find();
-      await queryRunner.commitTransaction();
-
-      return postDatas;
-    } catch (err) {
-      await queryRunner.rollbackTransaction();
-
-      throw err;
-    } finally {
-      await queryRunner.release();
-    }
+    return postCategories;
   }
 
+  @Transactional()
   public async getPostCategoryById(id: number): Promise<PostCategory> {
-    const queryRunner = this.dataSource.createQueryRunner();
+    const postCategory = await this.postCategoryRepository.findOne({ where: { id } });
 
-    await queryRunner.startTransaction();
+    if (!postCategory) throw new NotFoundException('데이터가 없습니다.');
 
-    try {
-      const postData = await this.postCategoryRepository.findOne({ where: { id } });
-      await queryRunner.commitTransaction();
-
-      return postData;
-    } catch (err) {
-      await queryRunner.rollbackTransaction();
-
-      throw new NotFoundException('사용자를 찾을 수 없습니다.');
-    } finally {
-      await queryRunner.release();
-    }
+    return postCategory;
   }
 }
