a946237c409272a6c4c5570896a5de9a1cffffd3
una3325 <a33256448@gmail.com>
2024-09-27 23:49:01 +0900

    style : 불필요한 주석 제거

--- apps/board/src/board/entities/board.entity.ts
+++ apps/board/src/board/entities/board.entity.ts
@@ -29,35 +29,4 @@ export class Board {
   @Column({ type: 'boolean', default: true })
   public is_open: boolean;
 
-  // @ManyToOne(()=>users)
-  // @JoinColumn({name:'user_id'})
-  // public user: User;
-
-  // @ManyToOne(()=>teams)
-  // @JoinColumn({name:'team_id'})
-  // public team: Team;
-
-  // @ManyToOne(() => PostCategory)
-  // @JoinColumn({name:'post_category_id'})
-  // public postCategory : PostCategory;
-
-  // @ManyToOne(() => ProjectCategory)
-  // @JoinColumn({name:'project_category_id'})
-  // public projectCategory : ProjectCategory;
-
-  // @ManyToOne(() => JobCategory)
-  // @JoinColumn({name:'job_category_id'})
-  // public jobCategory : JobCategory;
-
-  // @ManyToOne(() => StackCategory)
-  // @JoinColumn({name:'stack_category_id'})
-  // public stackCategory : StackCategory;
-
-  // @ManyToOne(() => CareerCategory)
-  // @JoinColumn({name:'career_category_id'})
-  // public careerCategory : CareerCategory;
-
-  // @ManyToOne(() => Attachment)
-  // @JoinColumn({name:'attachment_id'})
-  // public attachment : Attachment;
 }
