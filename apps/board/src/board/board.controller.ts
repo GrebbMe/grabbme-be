5e957e54afda314ac78df11b99e200c4212d827f
una3325 <a33256448@gmail.com>
2024-09-27 19:18:55 +0900

    refactor : 불필요한 컨트롤러 경로 제거

--- apps/board/src/board/board.controller.ts
+++ apps/board/src/board/board.controller.ts
@@ -4,7 +4,7 @@ import { BoardService } from './board.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
 
-@Controller('board')
+@Controller()
 export class BoardController {
   public constructor(private readonly boardService: BoardService) {}
 
