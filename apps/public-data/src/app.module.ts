6f0c935a1d30372ee4b160f5fe2a1fe3dcaf7857
YKDman <dbsrudejr300@gmail.com>
2024-09-29 21:51:47 +0900

    style : dataSourceFactory option 인수 설정 변경

--- apps/public-data/src/app.module.ts
+++ apps/public-data/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { DataSource, DataSourceOptions } from 'typeorm';
+import { DataSource } from 'typeorm';
 import { addTransactionalDataSource } from 'typeorm-transactional';
 import { PublicDataModule } from './public-data/public-data.module';
 import mysqlConfig from './config/mysql.config';
@@ -30,10 +30,13 @@ import mysqlConfig from './config/mysql.config';
         };
         return publicDataTypeOrmModuleOptions;
       },
-      dataSourceFactory: async (option: DataSourceOptions) => {
-        if (!option) throw new Error('Invalid Option Passes');
-        const dataSource = new DataSource(option);
-        return addTransactionalDataSource(dataSource);
+
+      dataSourceFactory: async (option?) => {
+        if (!option) {
+          throw new Error('Option is required');
+        }
+
+        return addTransactionalDataSource(new DataSource(option));
       },
     }),
 
