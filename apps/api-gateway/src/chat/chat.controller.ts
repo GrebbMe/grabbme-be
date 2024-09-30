07e980a5c2c9f28c6ae051f4bd7d42c4e204abd8
uzyhu <dnwlgn0807@naver.com>
2024-09-30 23:29:36 +0900

    refactor : dto 주입

--- apps/api-gateway/src/chat/chat.controller.ts
+++ apps/api-gateway/src/chat/chat.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ChatService } from './chat.service';
+import { CreateChatRoomDto } from './dto/req.dto';
 
 @Controller('chat')
 @ApiTags('Chat API')
 export class ChatController {
   public constructor(private readonly chatService: ChatService) {}
 
-  @Post('create-room')
-  public createChatRoom(@Body() createRoomDto: { roomName: string }) {
-    return this.chatService.createRoom(createRoomDto.roomName);
+  @Post('create-chat-room')
+  public createChatRoom(@Body() createChatRoomDto: CreateChatRoomDto) {
+    return this.chatService.createChatRoom(createChatRoomDto.name);
   }
 }
