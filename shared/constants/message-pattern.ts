553c92bde602b819d41e317756a48e627439657e
minzziPark <minzzi0068@gmail.com>
2024-10-01 14:44:38 +0900

    merge : conflict 해결

--- shared/constants/message-pattern.ts
+++ shared/constants/message-pattern.ts
@@ -35,15 +35,6 @@ export const MESSAGE = {
       DELETE_POST: { cmd: 'delete-post' },
     },
   },
-  POST_DATA: {
-    POST: {
-      GET_ALL_POST: { cmd: 'get-all-post' },
-      GET_ONE_POST: { cmd: 'get-one-post' },
-      CREATE_POST: { cmd: 'create-post' },
-      UPDATE_POST: { cmd: 'update-post' },
-      DELETE_POST: { cmd: 'delete-post' },
-    },
-  },
   USER: {
     CREATE_USER: { cmd: 'create-user' },
   },
