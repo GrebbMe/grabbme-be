3ad44149169577ed30c1763653e576ac7de215a9
minzziPark <minzzi0068@gmail.com>
2024-10-02 14:40:06 +0900

    feat : swagger operations 추가

--- apps/api-gateway/src/user/user.controller.ts
+++ apps/api-gateway/src/user/user.controller.ts
@@ -1,6 +1,13 @@
 import { CreateUserDto } from '@apps/user/src/user/dto/create-user.dto';
+import { User } from '@apps/user/src/user/entities/user.entity';
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UserService } from './user.service';
 @Controller('user')
 @ApiTags('User API')
@@ -10,6 +17,14 @@ export class UserController {
   @ApiOperation({ summary: '신규 사용자 추가' })
   @ApiCreatedResponse({
     description: '신규 사용자 추가',
+    type: User,
+  })
+  @ApiBadRequestResponse({
+    description: '잘못된 요청',
+  })
+  @ApiBody({
+    description: '신규 사용자 정보',
+    type: CreateUserDto,
   })
   @Post()
   public async createUser(@Body() createUserDto: CreateUserDto) {
--- apps/user/src/user/dto/create-user.dto.ts
+++ apps/user/src/user/dto/create-user.dto.ts
@@ -1,26 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { CareerCategory, PositionCategory, ProjectCategory } from '@publicData/entities';
 import { IsArray, IsEmail, IsNotEmpty, IsNumber, IsString, ArrayMaxSize } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   public nickname: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsEmail()
   public email: string;
 
+  @ApiProperty({ type: 'array', items: { type: 'number' } })
   @IsArray()
   @ArrayMaxSize(5)
   @IsNumber({}, { each: true })
   public stack_category_id: number[];
 
+  @ApiProperty({ type: 'number' })
   @IsNumber()
   public position_category_id: PositionCategory;
 
+  @ApiProperty({ type: 'number' })
   @IsNumber()
   public project_category_id: ProjectCategory;
 
+  @ApiProperty({ type: 'number' })
   @IsNumber()
   public career_category_id: CareerCategory;
 }
