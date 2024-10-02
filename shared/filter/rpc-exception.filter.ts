cebdcad2a810f4e433c57bb243b1e6e1de5a9849
YKDman <dbsrudejr300@gmail.com>
2024-10-02 16:56:08 +0900

    feat : microservice rpc exception filter 구현

---
+++ shared/filter/rpc-exception.filter.ts
@@ -0,0 +1,15 @@
+import { Catch, RpcExceptionFilter, ArgumentsHost } from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { CustomRpcException } from './custrom-rpc-exception';
+
+@Catch(CustomRpcException)
+export class MicroRpcExceptionFilter implements RpcExceptionFilter<CustomRpcException> {
+  public catch(exception: CustomRpcException, host: ArgumentsHost): Observable<unknown> {
+    return throwError(() => {
+      return {
+        message: exception.message,
+        status: exception.status,
+      };
+    });
+  }
+}
