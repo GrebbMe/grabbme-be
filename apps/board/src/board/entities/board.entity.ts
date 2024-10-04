aea1b5023c7608cff2f43c2318cfaddd9c0830d5
una3325 <a33256448@gmail.com>
2024-10-04 12:01:42 +0900

    feat : Board Entity 정의 및 module 등록

--- apps/board/src/board/entities/board.entity.ts
+++ apps/board/src/board/entities/board.entity.ts
@@ -1,9 +1,36 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  CareerCategory,
+  PositionCategory,
+  PostCategory,
+  ProjectCategory,
+  StackCategory,
+} from '@publicData/entities';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('posts')
 export class Board {
   @PrimaryGeneratedColumn()
   public post_id: number;
+
+  @ManyToOne(() => PostCategory)
+  @JoinColumn({ name: 'post_category_id' })
+  public postCategory: PostCategory;
+
+  @ManyToOne(() => ProjectCategory)
+  @JoinColumn({ name: 'project_category_id' })
+  public projectCategory: ProjectCategory;
+
+  @ManyToOne(() => PositionCategory)
+  @JoinColumn({ name: 'position_category_id' })
+  public positionCategory: PositionCategory;
+
+  @ManyToOne(() => StackCategory)
+  @JoinColumn({ name: 'stack_category_id' })
+  public stackCategory: StackCategory;
+
+  @ManyToOne(() => CareerCategory)
+  @JoinColumn({ name: 'career_category_id' })
+  public careerCategory: CareerCategory;
 
   @Column({ type: 'varchar' })
   public title: string;
