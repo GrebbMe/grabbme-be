293ce64bc26b32f9b1548ccf5fa89f03433b6d91
uzyhu <dnwlgn0807@naver.com>
2024-10-04 18:21:08 +0900

    refactor : import 위치 변경

--- apps/chat/src/chat/chat.service.spec.ts
+++ apps/chat/src/chat/chat.service.spec.ts
@@ -1,5 +1,5 @@
-import { getModelToken } from '@nestjs/mongoose';
 import { InternalServerErrorException } from '@nestjs/common';
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Model } from 'mongoose';
 import { ChatService } from './chat.service';
