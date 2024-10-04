8ce3fd151b84d3d3b059c1d47b85fa965dbf28cc
minzziPark <minzzi0068@gmail.com>
2024-10-04 15:42:13 +0900

    refactor : 수정된 dto로 엔티티별 객체찾는 로직으로 변경

--- apps/user/src/user/user.service.ts
+++ apps/user/src/user/user.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CareerCategory, PositionCategory, ProjectCategory } from '@publicData/entities';
 import { Repository } from 'typeorm';
 import { Transactional } from 'typeorm-transactional';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto } from './dto/req.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -10,11 +11,32 @@ export class UserService {
   public constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    @InjectRepository(PositionCategory)
+    private positionRepository: Repository<PositionCategory>,
+    @InjectRepository(ProjectCategory)
+    private projectRepository: Repository<ProjectCategory>,
+    @InjectRepository(CareerCategory)
+    private careerRepository: Repository<CareerCategory>,
   ) {}
 
   @Transactional()
   public async createUser(createUserDto: CreateUserDto): Promise<User> {
-    const newUser = this.userRepository.create(createUserDto);
+    const positionCategory = await this.positionRepository.findOne({
+      where: { position_category_id: createUserDto.position_category_id },
+    });
+    const projectCategory = await this.projectRepository.findOne({
+      where: { project_category_id: createUserDto.project_category_id },
+    });
+    const careerCategory = await this.careerRepository.findOne({
+      where: { career_category_id: createUserDto.career_category_id },
+    });
+
+    const newUser = this.userRepository.create({
+      ...createUserDto,
+      position_category_id: positionCategory,
+      project_category_id: projectCategory,
+      career_category_id: careerCategory,
+    });
     return this.userRepository.save(newUser);
   }
 }
