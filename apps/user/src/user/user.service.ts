0a56d6089e01fb9d93145bc2080a6a161cf772f5
minzziPark <minzzi0068@gmail.com>
2024-10-07 03:13:58 +0900

    feat : user microservice 사용자 정보 수정 api 구현

--- apps/user/src/user/user.service.ts
+++ apps/user/src/user/user.service.ts
@@ -4,7 +4,7 @@ import { CareerCategory, PositionCategory, ProjectCategory } from '@publicData/e
 import { CustomRpcException } from '@shared/filter/custom-rpc-exception';
 import { Repository } from 'typeorm';
 import { Transactional } from 'typeorm-transactional';
-import { CreateUserDto } from './dto/req.dto';
+import { CreateUserDto, UpdateUserDto } from './dto/req.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -63,5 +63,61 @@ export class UserService {
     }
 
     return result.affected && true;
+  }
+
+  @Transactional()
+  public async updateUser(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    const user: User = await this.userRepository.findOne({
+      where: { user_id: id },
+    });
+
+    if (!user) {
+      throw new CustomRpcException(HttpStatus.NOT_FOUND, '데이터가 없습니다.');
+    }
+
+    if (updateUserDto.position_category_id) {
+      const positionCategory = await this.positionRepository.findOne({
+        where: { position_category_id: updateUserDto.position_category_id },
+      });
+      if (!positionCategory) {
+        throw new CustomRpcException(
+          HttpStatus.BAD_REQUEST,
+          'position_category_id가 존재하지 않습니다.',
+        );
+      }
+      user.position_category_id = positionCategory;
+    }
+
+    if (updateUserDto.project_category_id) {
+      const projectCategory = await this.projectRepository.findOne({
+        where: { project_category_id: updateUserDto.project_category_id },
+      });
+      if (!projectCategory) {
+        throw new CustomRpcException(
+          HttpStatus.BAD_REQUEST,
+          'project_category_id가 존재하지 않습니다.',
+        );
+      }
+      user.project_category_id = projectCategory;
+    }
+
+    if (updateUserDto.career_category_id) {
+      const careerCategory = await this.careerRepository.findOne({
+        where: { career_category_id: updateUserDto.career_category_id },
+      });
+      if (!careerCategory) {
+        throw new CustomRpcException(
+          HttpStatus.BAD_REQUEST,
+          'career_category_id가 존재하지 않습니다.',
+        );
+      }
+      user.career_category_id = careerCategory;
+    }
+
+    if (updateUserDto.stack_category_id) {
+      user.stack_category_id = updateUserDto.stack_category_id;
+    }
+
+    return await this.userRepository.save(user);
   }
 }
