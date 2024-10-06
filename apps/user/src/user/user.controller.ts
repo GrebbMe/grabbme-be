680f90f1af8b2ec765ef126445b60f9370bf6748
minzziPark <minzzi0068@gmail.com>
2024-10-06 12:35:22 +0900

    feat : user service controller 작성

--- apps/user/src/user/user.controller.ts
+++ apps/user/src/user/user.controller.ts
@@ -2,7 +2,7 @@ import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { MESSAGE } from '@shared/constants/message-pattern';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/req.dto';
+import { CreateUserDto, GetUserDto } from './dto/req.dto';
 
 @Controller()
 export class UserController {
@@ -11,5 +11,10 @@ export class UserController {
   @MessagePattern(MESSAGE.USER.CREATE_USER)
   private async createUser(@Payload() createUserDto: CreateUserDto) {
     return await this.userService.createUser({ ...createUserDto });
+  }
+
+  @MessagePattern(MESSAGE.USER.GET_USER)
+  private async getUser(@Payload() getUserDto: GetUserDto) {
+    return await this.userService.getUser(getUserDto.id);
   }
 }
