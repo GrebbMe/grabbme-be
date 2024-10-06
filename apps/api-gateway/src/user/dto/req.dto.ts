bc1c90e76873a7243478012a421b4eebc9346423
minzziPark <minzzi0068@gmail.com>
2024-10-06 14:09:04 +0900

    feat : api-gateway dto 수정

--- apps/api-gateway/src/user/dto/req.dto.ts
+++ apps/api-gateway/src/user/dto/req.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 import { IsArray, IsEmail, IsNotEmpty, IsNumber, IsString, ArrayMaxSize } from 'class-validator';
 
 export class CreateUserDto {
@@ -32,11 +33,37 @@ export class CreateUserDto {
 }
 
 export class GetUserDto {
+  @Type(() => Number)
   @IsNumber()
   public id: number;
 }
 
 export class DeleteUserDto {
+  @Type(() => Number)
   @IsNumber()
   public id: number;
+}
+
+export class UpdateUserDto {
+  @Type(() => Number)
+  @IsNumber()
+  public id: number;
+
+  @ApiProperty({ type: 'array', items: { type: 'number' } })
+  @IsArray()
+  @ArrayMaxSize(5)
+  @IsNumber({}, { each: true })
+  public stack_category_id: number[];
+
+  @ApiProperty({ type: 'number' })
+  @IsNumber()
+  public position_category_id: number;
+
+  @ApiProperty({ type: 'number' })
+  @IsNumber()
+  public project_category_id: number;
+
+  @ApiProperty({ type: 'number' })
+  @IsNumber()
+  public career_category_id: number;
 }
