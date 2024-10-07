b554c31a46460d408396fda02884a7e1d3e5894c
yeopseung <kwak991021@gmail.com>
2024-10-07 23:53:12 +0900

    fix : 채팅 컨트롤러 경로 수정

--- apps/api-gateway/src/chat/chat.controller.ts
+++ apps/api-gateway/src/chat/chat.controller.ts
@@ -8,23 +8,23 @@ import { CreateChatRoomDto, GetChatListDto, GetChatRoomDto, GetChatRoomsDto } fr
 export class ChatController {
   public constructor(private readonly chatService: ChatService) {}
 
-  @Post('chat-rooms')
+  @Post('rooms')
   public createChatRoom(@Body() createChatRoomDto: CreateChatRoomDto) {
     return this.chatService.createChatRoom(createChatRoomDto.name);
   }
 
-  @Get('chat-rooms')
+  @Get('rooms')
   public getChatRooms(@Body() { id }: GetChatRoomsDto) {
     //TODO: id값은 userId로 인증/인가 절차 구현시 삭제 예정
     return this.chatService.getChatRooms(id);
   }
 
-  @Get('chat-rooms/:id')
+  @Get('rooms/:id')
   public getChatRoom(@Param() { id }: GetChatRoomDto) {
     return this.chatService.getChatRoom(id);
   }
 
-  @Get('chat-lists/:id')
+  @Get('lists/:id')
   public getChatList(@Param() { id }: GetChatListDto, @Query() { page }: GetChatListDto) {
     return this.chatService.getChatList(id, page);
   }
