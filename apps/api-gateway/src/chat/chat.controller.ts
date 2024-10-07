1d67a6078a0853a3c219349eba195140e5dec468
yeopseung <kwak991021@gmail.com>
2024-10-07 23:52:33 +0900

    comment : 채팅 컨트롤러 주석 수정

--- apps/api-gateway/src/chat/chat.controller.ts
+++ apps/api-gateway/src/chat/chat.controller.ts
@@ -15,7 +15,7 @@ export class ChatController {
 
   @Get('chat-rooms')
   public getChatRooms(@Body() { id }: GetChatRoomsDto) {
-    //! id값은 userId로 인증/인가 절차 구현시 삭제 예정
+    //TODO: id값은 userId로 인증/인가 절차 구현시 삭제 예정
     return this.chatService.getChatRooms(id);
   }
 
