f2aab64e6b95520636025f3b411c9bc2be6b7c97
una3325 <a33256448@gmail.com>
2024-10-09 08:42:18 +0900

    docs : api 예시 데이터 추가

--- apps/api-gateway/src/board/board.controller.ts
+++ apps/api-gateway/src/board/board.controller.ts
@@ -13,10 +13,24 @@ export class BoardController {
   public constructor(private readonly boardService: BoardService) {}
 
   @Get('category/:postCategoryId')
-  @ApiOperation({ summary: 'post_category_id 별 전체 게시글 조회' })
+  @ApiOperation({ summary: 'post_category_id 별 전체 게시글 전체 조회' })
   @ApiOkResponse({
-    description: 'post_category_id 별 전체 게시글 조회 성공',
-    example: [],
+    description: 'post_category_id 별 전체 게시글 전체 조회',
+    example: [
+      {
+        post_id: 1,
+        title: '프로젝트명',
+        content: '프로젝트 내용',
+        expired_at: '2024-10-12T00:00:00.000Z',
+        view_cnt: 0,
+        bookmarked_cnt: 0,
+        stack_category_id: ['1', '2'],
+        post_category_id: {
+          id: 2,
+          post_category_name: '그랩존',
+        },
+      },
+    ],
   })
   public async getPostsByPostCategory(@Param('postCategoryId') postCategoryId: number) {
     return await this.boardService.getPostsByPostCategory(postCategoryId);
@@ -24,7 +38,30 @@ export class BoardController {
 
   @Get(':id')
   @ApiOperation({ summary: '게시글 상세 조회' })
-  @ApiOkResponse({})
+  @ApiOkResponse({
+    description: '게시글 상세 조회',
+    example: {
+      post_id: 1,
+      title: '프로젝트명',
+      content: '프로젝트 내용',
+      create_at: '2024-10-08T06:16:30.000Z',
+      expired_at: '2024-10-12T00:00:00.000Z',
+      view_cnt: 0,
+      bookmarked_cnt: 0,
+      is_open: true,
+      project_category_id: ['3'],
+      stack_category_id: ['1', '4'],
+      post_category_id: {
+        id: 2,
+        post_category_name: '그랩존',
+      },
+      career_category_id: {
+        career_category_id: 1,
+        content: '0년차',
+      },
+      position_category_id: null,
+    },
+  })
   public async getPostById(@Param('id') id: number) {
     return await this.boardService.getPostById(id);
   }
@@ -33,7 +70,24 @@ export class BoardController {
   @ApiOperation({ summary: '게시글 생성' })
   @ApiCreatedResponse({
     description: '게시글 생성',
-    example: {},
+    example: {
+      title: '프로젝트명',
+      content: '프로젝트 소개',
+      expired_at: '2024-10-10T00:00:00.000Z',
+      project_category_id: [2, 4],
+      stack_category_id: [4, 5],
+      post_category_id: {
+        id: 2,
+      },
+      career_category_id: null,
+      position_category_id: null,
+      post_id: 15,
+      create_at: '2024-10-08T23:23:46.000Z',
+      update_at: '2024-10-08T23:23:46.000Z',
+      view_cnt: 0,
+      bookmarked_cnt: 0,
+      is_open: true,
+    },
   })
   public async createPost(
     @Param('postCategoryId') postCategoryId: number,
@@ -47,7 +101,24 @@ export class BoardController {
   @ApiOperation({ summary: '게시글 수정' })
   @ApiOkResponse({
     description: '게시글 수정',
-    example: {},
+    example: {
+      post_id: 6,
+      title: '프로젝트명',
+      content: '프로젝트 내용',
+      create_at: '2024-10-08T06:16:30.000Z',
+      update_at: '2024-10-08T23:34:23.000Z',
+      expired_at: '2024-10-12T00:00:00.000Z',
+      view_cnt: 0,
+      bookmarked_cnt: 0,
+      is_open: true,
+      project_category_id: [3],
+      stack_category_id: [1, 4],
+      career_category_id: {
+        career_category_id: 1,
+        content: '0년차',
+      },
+      position_category_id: null,
+    },
   })
   public async updatePost(@Param('id') id: number, @Body() updateBoardDto: UpdateBoardDto) {
     const payload = { id, updateBoardDto };
