deaa41687d82a30c50310d2b3ca354ed36ed93d1
YKDman <dbsrudejr300@gmail.com>
2024-10-08 17:50:15 +0900

    style : config index export 구조로 변경

--- apps/api-gateway/src/config/index.ts
+++ apps/api-gateway/src/config/index.ts
@@ -1,5 +1,6 @@
+import clientConfig from './client.config';
 import githubConfig from './github.config';
 import mongoConfig from './mongo.config';
 import mysqlConfig from './mysql.config';
 
-export { githubConfig, mongoConfig, mysqlConfig };
+export { githubConfig, mongoConfig, mysqlConfig, clientConfig };
