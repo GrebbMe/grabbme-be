0418c0fbadcedef52aa8495ec793d36aabc8b7f5
YKDman <dbsrudejr300@gmail.com>
2024-10-08 17:28:15 +0900

    feat : jwt-refresh strategy guard 구현

---
+++ apps/api-gateway/src/auth/strategies/jwt-refresh.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-jwt';
+import { AuthService } from '../auth.service';
+import { Payload } from '../types/jwt.type';
+
+// * Refresh Token 검증 및 유저 정보 반환
+@Injectable()
+export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+  public constructor(
+    private authService: AuthService,
+    private configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: (req) => req.cookies.refreshToken ?? req.headers['x-refresh-token'],
+      ignoreExpiration: false,
+      secretOrKey: configService.get('jwt.secret'),
+    });
+  }
+
+  public async validate(payload: Payload) {
+    const user = await this.authService.validateUser(payload.email);
+
+    if (!user) {
+      throw new UnauthorizedException('유효하지 않은 사용자 입니다.');
+    }
+
+    return user;
+  }
+}
