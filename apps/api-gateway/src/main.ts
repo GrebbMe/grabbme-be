36d71b72e74120daba7f3fb59a3af3d23763be97
YKDman <dbsrudejr300@gmail.com>
2024-10-08 17:27:12 +0900

    config : api-gateway cors 및 cookie 설정

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -2,17 +2,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { GatewayRpcExceptionFilter } from './common/filter/gateway-rpc-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
   const configService = app.get(ConfigService);
   const httpAdapterHost = app.get(HttpAdapterHost);
+
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: process.env.CLIENT_MAIN_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     preflightContinue: false,
     credentials: true,
   });
@@ -22,7 +23,6 @@ async function bootstrap() {
     .setTitle('GrabbMe API Docs')
     .setDescription('GrabbMe Backend API Documents')
     .setVersion('1.0.0')
-    .addBearerAuth()
     .addServer('api')
     .build();
 
@@ -31,6 +31,9 @@ async function bootstrap() {
 
   // root Endpoint 설정
   app.setGlobalPrefix('api');
+
+  // cookie 설정
+  app.use(cookieParser());
 
   // API 통신을 위한 validator
   app.useGlobalPipes(
