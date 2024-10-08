f7e7623e8bbac63428ab5d821ad9abdef29d76b3
una3325 <a33256448@gmail.com>
2024-10-09 07:41:03 +0900

    docs : dto에 ApiProperty 추가

--- apps/api-gateway/src/board/dto/req.dto.ts
+++ apps/api-gateway/src/board/dto/req.dto.ts
@@ -1,34 +1,52 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsArray, IsDate, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import {
+  ArrayMaxSize,
+  IsArray,
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class CreateBoardDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   public title: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   public content: string;
 
+  @ApiProperty({ type: 'array', items: { type: 'number' } })
   @IsArray()
+  @ArrayMaxSize(5)
   @IsNumber({}, { each: true })
   @IsNotEmpty()
   public project_category_id: number[];
 
+  @ApiProperty({ type: 'array', items: { type: 'number' } })
   @IsArray()
+  @ArrayMaxSize(3)
   @IsNumber({}, { each: true })
   @IsNotEmpty()
   public stack_category_id: number[];
 
+  @ApiProperty({ type: 'number' })
   @IsNumber()
   @IsOptional()
   public position_category_id?: number;
 
+  @ApiProperty({ type: 'number' })
   @IsNumber()
   @IsOptional()
   public career_category_id?: number;
 
+  @ApiProperty({ type: 'Date' })
   @Type(() => Date)
   @IsDate()
   @IsOptional()
@@ -36,5 +54,8 @@ export class CreateBoardDto {
 }
 
 export class UpdateBoardDto extends PartialType(CreateBoardDto) {
+  @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
   public id: number;
 }
--- apps/board/src/board/dto/req.dto.ts
+++ apps/board/src/board/dto/req.dto.ts
@@ -1,34 +1,52 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsArray, IsDate, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import {
+  ArrayMaxSize,
+  IsArray,
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class CreateBoardDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   public title: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   public content: string;
 
+  @ApiProperty({ type: 'array', items: { type: 'number' } })
   @IsArray()
+  @ArrayMaxSize(5)
   @IsNumber({}, { each: true })
   @IsNotEmpty()
   public project_category_id: number[];
 
+  @ApiProperty({ type: 'array', items: { type: 'number' } })
   @IsArray()
+  @ArrayMaxSize(3)
   @IsNumber({}, { each: true })
   @IsNotEmpty()
   public stack_category_id: number[];
 
+  @ApiProperty({ type: 'number' })
   @IsNumber()
   @IsOptional()
   public position_category_id?: number;
 
+  @ApiProperty({ type: 'number' })
   @IsNumber()
   @IsOptional()
   public career_category_id?: number;
 
+  @ApiProperty({ type: 'Date' })
   @Type(() => Date)
   @IsDate()
   @IsOptional()
@@ -36,34 +54,45 @@ export class CreateBoardDto {
 }
 
 export class UpdateBoardDto extends PartialType(CreateBoardDto) {
+  @ApiProperty()
   @IsNumber()
   public id: number;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   public title?: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   public content?: string;
 
-  @IsDate()
-  @IsOptional()
-  public expired_at?: Date;
-
+  @ApiProperty({ type: 'array', items: { type: 'number' } })
+  @IsArray()
   @IsNumber({}, { each: true })
   @IsOptional()
   public project_category_id?: number[];
 
+  @ApiProperty({ type: 'array', items: { type: 'number' } })
+  @IsArray()
+  @ArrayMaxSize(3)
   @IsNumber({}, { each: true })
   @IsOptional()
   public stack_category_id?: number[];
 
+  @ApiProperty({ type: 'number' })
   @IsNumber()
   @IsOptional()
   public career_category_id?: number;
 
+  @ApiProperty({ type: 'number' })
   @IsNumber()
   @IsOptional()
   public position_category_id?: number;
+
+  @ApiProperty()
+  @IsDate()
+  @IsOptional()
+  public expired_at?: Date;
 }
