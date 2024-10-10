ecd184fed58b24143a441ed94196d3c2e620ae52
una3325 <a33256448@gmail.com>
2024-10-11 04:00:49 +0900

    refactor : UpdateBoardDto 불필요한 필드 정의 제거

--- apps/board/src/board/dto/req.dto.ts
+++ apps/board/src/board/dto/req.dto.ts
@@ -53,43 +53,4 @@ export class CreateBoardDto {
   public expired_at?: Date;
 }
 
-export class UpdateBoardDto extends PartialType(CreateBoardDto) {
-  @ApiProperty()
-  @IsString()
-  @IsOptional()
-  public title?: string;
-
-  @ApiProperty()
-  @IsString()
-  @IsOptional()
-  public content?: string;
-
-  @ApiProperty({ type: 'array', items: { type: 'number' } })
-  @IsArray()
-  @ArrayMaxSize(3)
-  @IsNumber({}, { each: true })
-  @IsOptional()
-  public project_category_id?: number[];
-
-  @ApiProperty({ type: 'array', items: { type: 'number' } })
-  @IsArray()
-  @ArrayMaxSize(5)
-  @IsNumber({}, { each: true })
-  @IsOptional()
-  public stack_category_id?: number[];
-
-  @ApiProperty({ type: 'number' })
-  @IsNumber()
-  @IsOptional()
-  public career_category_id?: number;
-
-  @ApiProperty({ type: 'number' })
-  @IsNumber()
-  @IsOptional()
-  public position_category_id?: number;
-
-  @ApiProperty()
-  @IsDate()
-  @IsOptional()
-  public expired_at?: Date;
-}
+export class UpdateBoardDto extends PartialType(CreateBoardDto) {}
