f87f7acd35461ed24b684974fa84b9d6375159f6
YKDman <dbsrudejr300@gmail.com>
2024-10-12 09:57:35 +0900

    config : jwt accesstoken expire time 15m setting

--- apps/api-gateway/src/config/jwt.config.ts
+++ apps/api-gateway/src/config/jwt.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('jwt', () => {
   return {
     secret: process.env.JWT_SECRET,
-    accessSignOptions: { expiresIn: '1m' },
+    accessSignOptions: { expiresIn: '15m' },
     refreshSignOptions: { expiresIn: '7d' },
   };
 });
