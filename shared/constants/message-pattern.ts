f4d2f3ccf2835b7a9e7dd561d0dea62289fa667b
una3325 <a33256448@gmail.com>
2024-10-14 02:47:12 +0900

    refactor : message-pattern 구조 수정

--- shared/constants/message-pattern.ts
+++ shared/constants/message-pattern.ts
@@ -26,14 +26,12 @@ export const MESSAGE = {
     },
   },
 
-  POST_DATA: {
-    POST: {
-      GET_ALL_POST_BY_POST_CATEGORY_ID: { cmd: 'get-all-post-by-post-category-id' },
-      GET_ONE_POST: { cmd: 'get-one-post' },
-      CREATE_POST: { cmd: 'create-post' },
-      UPDATE_POST: { cmd: 'update-post' },
-      DELETE_POST: { cmd: 'delete-post' },
-    },
+  POST: {
+    GET_ALL_POST_BY_POST_CATEGORY_ID: { cmd: 'get-all-post-by-post-category-id' },
+    GET_ONE_POST: { cmd: 'get-one-post' },
+    CREATE_POST: { cmd: 'create-post' },
+    UPDATE_POST: { cmd: 'update-post' },
+    DELETE_POST: { cmd: 'delete-post' },
   },
 
   CHAT: {
