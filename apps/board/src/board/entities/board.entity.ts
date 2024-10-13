9ae6ad5c51a4e1895c7448cb30baab99055a4d2b
yuna <115125675+una3325@users.noreply.github.com>
2024-10-14 01:45:36 +0900

    [FEAT] board 서비스 post_category_id에 따른 분기 처리 (#54)
    * feat : Board Entity 정의 및 module 등록
    
    * feat : post-category-id 별 게시글 api 구현
    
    * db : posts entity 정의
    
    * move : posts dto 구조 변경
    
    * refactor : 함수명 및 파일 경로 변경
    
    * feat : post-category-id 별 게시글 조회 및 생성
    
    * refactor : message-pattern 상수명 변경
    
    * refactor : createPost 및 updatePost 로직 변경
    
    * docs : dto에 ApiProperty 추가
    
    * bug : dto 수정
    
    * refactor : api 응답 필드 수정
    
    * docs : api 예시 데이터 추가
    
    * fix : dto ArrayMaxSize 배열 최대 크기 수정
    
    * refactor : 카테고리 별 게시글 조회 함수명 변경
    
    * refactor : UpdateBoardDto 불필요한 필드 정의 제거
    
    * refactor : 불필요한 if문 제거
    
    * docs : api 예시 데이터 수정
    
    * refactor : classToPlain 적용 응답 데이터 평문 변환

--- apps/board/src/board/entities/board.entity.ts
+++ apps/board/src/board/entities/board.entity.ts
@@ -1,4 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { CareerCategory, PositionCategory, PostCategory } from '@publicData/entities';
+import { Transform } from 'class-transformer';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('posts')
 export class Board {
@@ -11,13 +13,16 @@ export class Board {
   @Column({ type: 'text' })
   public content: string;
 
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
-  public update_at: Date;
-
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @Transform(({ value }) => (value ? value.toISOString().split('T')[0] : null))
   public create_at: Date;
 
-  @Column({ type: 'timestamp' })
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  @Transform(({ value }) => (value ? value.toISOString().split('T')[0] : null))
+  public update_at: Date;
+
+  @Column({ type: 'timestamp', default: null, nullable: true })
+  @Transform(({ value }) => (value ? value.toISOString().split('T')[0] : null))
   public expired_at: Date;
 
   @Column({ type: 'int', default: 0 })
@@ -28,4 +33,23 @@ export class Board {
 
   @Column({ type: 'boolean', default: true })
   public is_open: boolean;
+
+  @ManyToOne(() => PostCategory)
+  @JoinColumn({ name: 'post_category_id' })
+  @Transform(({ value }) => value?.id)
+  public post_category_id: PostCategory;
+
+  @ManyToOne(() => CareerCategory, { nullable: true })
+  @JoinColumn({ name: 'career_category_id' })
+  public career_category_id?: CareerCategory;
+
+  @ManyToOne(() => PositionCategory, { nullable: true })
+  @JoinColumn({ name: 'position_category_id' })
+  public position_category_id?: PositionCategory;
+
+  @Column('simple-array')
+  public project_category_id: number[];
+
+  @Column('simple-array')
+  public stack_category_id: number[];
 }
